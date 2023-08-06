f0b74dfbd7cd85124384c74b2928529eb0d98175
Anthony Hadwan <anthony.hadwan@hec.edu>
2023-08-06 17:56:32 +0200

    added code to determine restaurant availability

--- Udemy-Next13/pages/api/restaurant/[slug]/availability.ts
+++ Udemy-Next13/pages/api/restaurant/[slug]/availability.ts
@@ -46,5 +46,60 @@ export default async function handler(
     },
   });
 
-  return res.json({ searchTimes, bookings });
+  const bookingTablesObj: { [key: string]: { [key: number]: true } } = {};
+  bookings.forEach((booking) => {
+    bookingTablesObj[booking.booking_time.toISOString()] =
+      booking.tables.reduce((obj, table) => {
+        return {
+          ...obj,
+          [table.table_id]: true,
+        };
+      }, {});
+  });
+
+  const restaurant = await prisma.restaurant.findUnique({
+    where: {
+      slug,
+    },
+    select: {
+      tables: true,
+    },
+  });
+
+  if (!restaurant) {
+    return res.status(400).json({
+      errorMessage: 'Invalid data provided',
+    });
+  }
+
+  const tables = restaurant.tables;
+
+  const searchTimesWithTables = searchTimes.map((searchTime) => {
+    return {
+      date: new Date(`${day}T${searchTime}`),
+      time: searchTime,
+      tables,
+    };
+  });
+
+  searchTimesWithTables.forEach((t) => {
+    t.tables = t.tables.filter((table) => {
+      if (bookingTablesObj[t.date.toISOString()]) {
+        if (bookingTablesObj[t.date.toISOString()][table.id]) return false;
+      }
+    });
+  });
+
+  const availabilities = searchTimesWithTables.map((t) => {
+    const sumSeats = t.tables.reduce((sum, table) => {
+      return sum + table.seats;
+    }, 0);
+
+    return {
+      time: t.time,
+      availabilities: sumSeats >= parseInt(partySize),
+    };
+  });
+
+  return res.json({ searchTimes, bookings, bookingTablesObj, tables });
 }
